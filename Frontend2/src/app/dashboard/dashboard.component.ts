7dd59cfb7a58838b507dbe5d62ac862a7ca3c0e1
swapnilghule45 <ghuleswapnil46@gmail.com>
2023-10-12 03:57:19 +0530

    Appointment

--- Frontend2/src/app/dashboard/dashboard.component.ts
+++ Frontend2/src/app/dashboard/dashboard.component.ts
@@ -73,7 +73,9 @@ export class DashboardComponent implements OnInit {
       const dataFromDatabase = [
         { id: 1, startTime: '2023-10-12T03:50:00', endTime: '2023-10-15T14:00:00', otherFields: '...' },
         { id: 2, startTime: '2023-10-12T01:00:00', endTime: '2023-10-12T02:00:00', otherFields: '...' },
-        { id: 3, startTime: '2023-10-12T00:01:00', endTime: '2023-10-12T00:03:00', otherFields: '...' }
+        { id: 3, startTime: '2023-10-12T00:01:00', endTime: '2023-10-12T00:03:00', otherFields: '...' },
+        { id: 4, startTime: '2023-10-12T02:01:00', endTime: '2023-10-12T04:03:00', otherFields: '...' }
+
         // ...
       ];
       
--- Frontend2/src/app/user-profile/user-profile.component.ts
+++ Frontend2/src/app/user-profile/user-profile.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
+  userAppointment: any[];
   constructor() { }
 
   ngOnInit() {
+ 
+    this.userAppointment=[];
+    this.userAppointment=[ { clientName: 'ABC', startTime: '2023-10-12T03:50:00', endTime: '2023-10-15T14:00:00', Address: '...', Phone:'1234' },
+    { clientName: 'ABC', startTime: '2023-10-12T03:50:00', endTime: '2023-10-15T14:00:00', Address: '...', Phone:'1234' },
+    { clientName: 'ABC', startTime: '2023-10-12T03:50:00', endTime: '2023-10-15T14:00:00', Address: '...', Phone:'1234' },
+    { clientName: 'ABC', startTime: '2023-10-12T03:50:00', endTime: '2023-10-15T14:00:00', Address: '...', Phone:'1234' }
+  ]
   }
 
 }
