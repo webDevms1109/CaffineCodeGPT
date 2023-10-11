cd7bfaadfc678085299e0a51e39695d20e4346f9
Geekaddict <shreyarani73@gmail.com>
2023-10-12 04:21:05 +0530

    email

--- Backend/Codefest/therapist/views.py
+++ Backend/Codefest/therapist/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
 
+import smtplib
+
 # Create your views here.
 def home(request):
     return HttpResponse("Therapist")
@@ -11,4 +13,25 @@ def checkOut(request):
     return HttpResponse("Check Out")
 
 def sos(request):
+    # creates SMTP session
+    sender_email_id = "listening_ear1@outlook.com"
+    sender_email_id_password = "Hello@12345"
+    receiver_email_id = "administrator.models.email"
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    
+    # start TLS for security
+    s.starttls()
+    
+    # Authentication
+    s.login("sender_email_id", "sender_email_id_password")
+    
+    # message to be sent
+    message = "Help I am in Danger!"
+    
+    # sending the mail
+    s.sendmail("sender_email_id", "receiver_email_id", message)
+    
+    # terminating the session
+    s.quit()
     return HttpResponse("SOS")
