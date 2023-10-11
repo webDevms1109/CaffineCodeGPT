d7fa349759b1620ac8bdfd711479ac1db365ce4d
Priyanka Asrani <priyankaasrani@Priyankas-MacBook-Air.local>
2023-10-11 20:11:22 +0530

    Basic endpoints backend

--- Backend/Codefest/therapist/urls.py
+++ Backend/Codefest/therapist/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("checkIn", views.checkIn, name="checkIn"),
+    path("checkOut", views.checkOut, name="checkOut"),
+    path("sos", views.sos, name="sos")
 ]
--- Backend/Codefest/therapist/views.py
+++ Backend/Codefest/therapist/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Therapist")
+
+def checkIn(request):
+    return HttpResponse("Check In")
+
+def checkOut(request):
+    return HttpResponse("Check Out")
+
+def sos(request):
+    return HttpResponse("SOS")
