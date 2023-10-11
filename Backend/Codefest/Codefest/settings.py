bf8eb4054946d5be3985c40b8b20ae37829fae5e
Priyanka Asrani <priyankaasrani@Priyankas-MacBook-Air.local>
2023-10-11 22:50:20 +0530

    Added client model

--- Backend/Codefest/Codefest/settings.py
+++ Backend/Codefest/Codefest/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'administrator',
-    'therapist'
+    'therapist',
+    'appointment',
+    'client'
 ]
 
 MIDDLEWARE = [
