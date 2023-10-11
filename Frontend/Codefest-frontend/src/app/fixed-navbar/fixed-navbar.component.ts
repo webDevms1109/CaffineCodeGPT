3235c22fc341bf0a68708ae93eb92ad337cf41b2
anshikamahajan <anshikamahajan12345@gmail.com>
2023-10-12 04:53:10 +0530

    api

---
+++ Frontend/Codefest-frontend/src/app/fixed-navbar/fixed-navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fixed-navbar',
+  templateUrl: './fixed-navbar.component.html',
+  styleUrls: ['./fixed-navbar.component.scss']
+})
+export class FixedNavbarComponent {
+
+}
