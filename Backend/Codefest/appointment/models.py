21631522ce35696af6ccea890cccbc04314b74a6
Priyanka Asrani <priyankaasrani@Priyankas-MacBook-Air.local>
2023-10-11 23:59:53 +0530

    Fixed models

--- Backend/Codefest/appointment/models.py
+++ Backend/Codefest/appointment/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from administrator.models import Administrator
 from therapist.models import Therapist
-from client.models import Client
+#from client.models import Client
 
 # Create your models here.
 class Appointment(models.Model):
@@ -9,7 +9,6 @@ class Appointment(models.Model):
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     status = models.CharField(max_length=30)
-    client_name = models.ForeignKey(Client, on_delete=models.CASCADE)
     therapist_name = models.ForeignKey(Therapist, on_delete=models.CASCADE)
     admin_name = models.ForeignKey(Administrator, on_delete=models.CASCADE)
 
