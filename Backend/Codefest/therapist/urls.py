2dce9ef59c64b4d5237032cafda6995719ef2022
Rajat Pattanaik <2001rajatpattanaik@gmail.com>
2023-10-12 13:01:13 +0530

    help

--- Backend/Codefest/therapist/urls.py
+++ Backend/Codefest/therapist/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("<str:slug>", views.home, name="home"),
     path("checkIn/<str:slug>", views.checkIn, name="checkIn"),
     path("checkOut/<str:slug>", views.checkOut, name="checkOut"),
-    path("sos", views.sos, name="sos")
+    path("email/sos", views.sos, name="sos")
 ]
